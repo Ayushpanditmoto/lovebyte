9cc9e4020715f51f69ccb4ae0ee9816a568392fd
Ayush Pandit <ayushpanditmoto@gmail.com>
2023-05-24 20:17:36 +0530

    feat working

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,18 +1,15 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { useRouter } from 'next/router';
-import { auth } from '../firebase/config';
-import { AiFillCopy } from 'react-icons/ai';
-import { AuthContext } from '@/context/authContext';
-import Love from '@/types/lovetype';
-import Spinner from './spinner';
-
-
+import React, { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import { auth } from "../firebase/config";
+import { AiFillCopy } from "react-icons/ai";
+import { AuthContext } from "@/context/authContext";
+import Love from "@/types/lovetype";
+import Spinner from "./spinner";
 
 function HomePage() {
   let uid: string;
 
-
-  const { getLove ,loading} = useContext(AuthContext);
+  const { getLove, loading } = useContext(AuthContext);
   const router = useRouter();
   const [love, setLove] = useState<Love[]>([]);
   const [shareableLink, setShareableLink] = useState<string>("");
@@ -26,9 +23,8 @@ function HomePage() {
     uid = auth.currentUser?.uid ?? "404";
     setShareableLink(`https://lovebytes.vercel.app/${uid}`);
 
-   fetchLove();
+    fetchLove();
   }, []);
-
 
   const handleCopy = () => {
     navigator.clipboard.writeText(shareableLink);
@@ -40,13 +36,18 @@ function HomePage() {
   }
 
   return (
-    <div className="wrapper w-screen h-screen bg-cover" style={{ backgroundImage: "url(love.jpg)" }}>
+    <div
+      className="wrapper w-screen h-screen bg-cover"
+      style={{ backgroundImage: "url(love.jpg)" }}
+    >
       <div className="flex flex-col items-center p-5">
         <h1 className="text-4xl font-bold text-white">Love ♥ Calculator</h1>
       </div>
       <div className="flex flex-col items-center p-5 ">
         <h1 className="text-2xl font-bold text-white">Shareable Link</h1>
-        <p className="bg-gray-600 rounded-md p-5 text-white whitespace-normal break-all w-auto">{shareableLink}</p>
+        <p className="bg-gray-600 rounded-md p-5 text-white whitespace-normal break-all w-auto">
+          {shareableLink}
+        </p>
         <button
           onClick={handleCopy}
           className="bg-red-500  hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-3"
@@ -55,30 +56,24 @@ function HomePage() {
           Copy Link
         </button>
       </div>
-      
 
-      {love.map((love) => (
-        <div className="flex flex-col m-1 bg-lime-200 items-center p-5">
-        <table className="w-full">
-    <tr>
-      <td className="px-4 py-2">Your Love</td>
-      <td className="px-4 py-2">:</td>
-      <td className="px-4 py-2">{love.name}</td>
-    </tr>
-    <tr>
-      <td className="px-4 py-2">Your Crush</td>
-      <td className="px-4 py-2">:</td>
-      <td className="px-4 py-2">{love.partner}</td>
-    </tr>
-    <tr>
-      <td className="px-4 py-2">Your Love Percentage</td>
-      <td className="px-4 py-2">:</td>
-      <td className="px-4 py-2">{love.message}</td>
-    </tr>
-  </table>
+      {love.map((loves, index) => (
+        <div className="flex flex-col m-5 rounded-md bg-lime-200 items-center p-5 relative">
+          <div className="absolute -top-4 -left-4 rounded-full bg-blue-500 text-white w-8 h-8 flex items-center justify-center">
+            <span className="text-sm">{love.length - index}</span>
+          </div>
+          <table className="w-full table-auto">
+            <tr>
+              <td className="px-4 py-2">Name</td>
+              <td className="px-4 py-2">Crush</td>
+            </tr>
+            <tr>
+              <td className="px-4 py-2">{loves.name}</td>
+              <td className="px-4 py-2">{loves.partner}</td>
+            </tr>
+          </table>
         </div>
       ))}
-
     </div>
   );
 }
